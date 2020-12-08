0807e436e765e4ed6d85070be4b4b0f4f2bc623d
dcajasn <61527316+dcajasn@users.noreply.github.com>
2020-12-08 18:19:36 -0500

    update docs

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ PACKAGES = [
 CLASSIFIERS = [
     'Intended Audience :: Financial and Insurance Industry',
     'Intended Audience :: Science/Research',
-    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
