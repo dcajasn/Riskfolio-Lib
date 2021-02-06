1851ea07142b0ab5cd649ea7371dbb136bae3094
dcajasn <61527316+dcajasn@users.noreply.github.com>
2021-02-06 00:14:12 -0500

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ author = 'Dany Cajas'
 # The short X.Y version
 version = 'latest'
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
