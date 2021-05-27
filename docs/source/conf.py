9ca84baafd8c0b0861bb747d5fe15a8b724c4b2c
dcajasn <61527316+dcajasn@users.noreply.github.com>
2021-05-27 01:45:18 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,10 +42,14 @@ release = '0.3.0.1'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
     'sphinx.ext.napoleon',
     'sphinxcontrib.bibtex',
     'sphinxemoji.sphinxemoji',
