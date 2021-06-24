b37d1fb0287a8659e4b0fb4abb62feb89fa10106
dcajasn <61527316+dcajasn@users.noreply.github.com>
2021-06-24 11:30:59 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-# import sphinx_rtd_theme
+import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(1, os.path.abspath('../../riskfolio'))
@@ -37,7 +37,7 @@ release = '0.3.0.1'
 # ones.
 extensions = [
 #    'numpydoc',
-#    'sphinx_rtd_theme',
+    'sphinx_rtd_theme',
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.autosummary',
@@ -79,7 +79,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
