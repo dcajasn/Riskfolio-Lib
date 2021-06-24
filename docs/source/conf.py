7e02b1f2c960f922729bb72acf42848e5d52da12
dcajasn <61527316+dcajasn@users.noreply.github.com>
2021-06-24 03:08:19 -0500

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,9 +14,9 @@
 #
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(1, os.path.abspath('../../riskfolio'))
-print(sys.path)
 
 
 # -- Project information -----------------------------------------------------
