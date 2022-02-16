3bbf7dc3d360fa1768f7d07a7934c67c03997df2
dcajasn <61527316+dcajasn@users.noreply.github.com>
2022-02-16 15:44:20 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'Dany Cajas'
 # The short X.Y version
 version = 'latest'
 # The full version, including alpha/beta/rc tags
-release = '2.0.1'
+release = '3.0.0'
 
 
 # -- General configuration ---------------------------------------------------
