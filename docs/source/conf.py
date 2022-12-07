7e0fcb190482d9109d5fb6839911deb7a2fe9459
Riskfolio <dany.cajas.n@uni.pe>
2022-12-07 00:18:05 -0500

    Fix docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,6 +56,7 @@ extensions = [
      'sphinxemoji.sphinxemoji',
 ]
 
+autodoc_mock_imports = ["_cppfunctions"]
 autodoc_member_order = 'bysource'
 autosummary_generate = True
 keep_warnings = False
